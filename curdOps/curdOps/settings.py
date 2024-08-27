2c3502318944ac6b6e80d49f340f68344755c1f3
piyushvijay28 <piyushvj2002@gmail.com>
2024-08-27 20:07:56 +0530

    new commit

--- curdOps/curdOps/settings.py
+++ curdOps/curdOps/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b-!_l=)xcu%j0zs8n&81xm!r5u38dpz4xt*wg3iipdi%-p$!b=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
